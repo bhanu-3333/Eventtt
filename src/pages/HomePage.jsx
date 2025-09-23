e377124aa7d0f52a2a11155673d671f1e5e52d9f
bhanu-3333 <bhanuu339@gmail.com>
2025-09-23 20:20:18 +0530

    packages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import MovieCard from "../components/MovieCard";
 
-const movies = [
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
+// Sample movies (new releases)
+const newReleases = [
   { id: 1, title: "Avengers: Endgame", poster: "/poster1.jpg", theatre: "IMAX Theatre", showtime: "7:30 PM", price: 150 },
   { id: 2, title: "Spider-Man: No Way Home", poster: "/poster2.jpg", theatre: "PVR Cinemas", showtime: "6:00 PM", price: 120 },
   { id: 3, title: "The Batman", poster: "/poster3.jpg", theatre: "Cinepolis", showtime: "8:00 PM", price: 130 },
+  { id: 4, title: "Guardians of the Galaxy 3", poster: "/poster4.jpg", theatre: "INOX", showtime: "9:00 PM", price: 140 },
+  { id: 5, title: "Black Panther 2", poster: "/poster5.jpg", theatre: "PVR Cinemas", showtime: "7:00 PM", price: 150 },
 ];
 
 export default function HomePage() {
   return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div style={{ padding: "20px" }}>
+      <h2>New Releases</h2>
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={20}
+        slidesPerView={3}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        loop={true}
+        pagination={{ clickable: true }}
+      >
+        {newReleases.map(movie => (
+          <SwiperSlide key={movie.id}>
+            <MovieCard movie={movie} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
