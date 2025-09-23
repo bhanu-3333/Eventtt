001d728bebe30ab4e6d24eb8cd9ff2e2656719b3
bhanu-3333 <bhanuu339@gmail.com>
2025-09-23 19:53:16 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import MovieDetailPage from './pages/MovieDetailPage';
-import SeatSelection from './pages/SeatSelection';
-import BookingSummary from './pages/BookingSummary';
-import Confirmation from './pages/Confirmation';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import SeatSelection from "./pages/SeatSelection";
+import SnackSelection from "./pages/SnackSelection";
+import BookingSummary from "./pages/BookingSummary";
+import Confirmation from "./pages/Confirmation";
+
+export default function App() {
+  const [bookingData, setBookingData] = useState({
+    movie: null,
+    seats: [],
+    snacks: [],
+    total: 0,
+    theatre: "",
+    showtime: "",
+    id: Date.now()
+  });
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movie/:id" element={<MovieDetailPage />} />
-        <Route path="/seat-selection/:id/:showtime" element={<SeatSelection />} />
-        <Route path="/summary/:id/:showtime" element={<BookingSummary />} />
-        <Route path="/confirmation/:bookingId" element={<Confirmation />} />
+        <Route path="/seats" element={<SeatSelection bookingData={bookingData} setBookingData={setBookingData} />} />
+        <Route path="/snacks" element={<SnackSelection bookingData={bookingData} setBookingData={setBookingData} />} />
+        <Route path="/summary" element={<BookingSummary bookingData={bookingData} setBookingData={setBookingData} />} />
+        <Route path="/confirmation" element={<Confirmation bookingData={bookingData} />} />
       </Routes>
     </>
   );
 }
-
-export default App;
